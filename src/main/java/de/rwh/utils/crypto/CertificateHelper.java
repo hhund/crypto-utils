00d18ae8c9e4624cf2df56a5d8eb641cba0924d3
hhund <hhund@6f6725fb-43f8-443a-aa18-9a63a978c7b8>
2013-02-08 18:34:44 +0000

    new trust store extraction Method

--- src/main/java/de/rwh/utils/crypto/CertificateHelper.java
+++ src/main/java/de/rwh/utils/crypto/CertificateHelper.java
@@ -15,6 +15,10 @@ import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.UUID;
 
 import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
@@ -123,5 +127,39 @@ public final class CertificateHelper
 		keyStore.load(null, null);
 		keyStore.setKeyEntry(certificateAlias, privateKey, password.toCharArray(), certificate);
 		return keyStore;
+	}
+
+	public static KeyStore extractTrust(KeyStore keyStoreIn) throws KeyStoreException, NoSuchAlgorithmException,
+			CertificateException, IOException
+	{
+		KeyStore keyStore = KeyStore.getInstance("jks");
+		keyStore.load(null, null);
+		for (X509Certificate caCert : getCaCertificates(keyStoreIn))
+			keyStore.setCertificateEntry(UUID.randomUUID().toString(), caCert);
+
+		return keyStore;
+	}
+
+	private static List<X509Certificate> getCaCertificates(KeyStore s) throws KeyStoreException
+	{
+		List<X509Certificate> caCertificates = new ArrayList<>();
+		for (Enumeration<String> e = s.aliases(); e.hasMoreElements();)
+		{
+			String alias = e.nextElement();
+			Certificate[] chain = s.getCertificateChain(alias);
+			if (chain == null)
+				chain = new Certificate[] { s.getCertificate(alias) };
+
+			for (Certificate c : chain)
+			{
+				if (c instanceof X509Certificate)
+				{
+					X509Certificate x = (X509Certificate) c;
+					if (x.getBasicConstraints() >= 0)
+						caCertificates.add(x);
+				}
+			}
+		}
+		return caCertificates;
 	}
 }
