bca61e4cc48ff096ae9eee78801faedf91eedd35
hhund <hhund@6f6725fb-43f8-443a-aa18-9a63a978c7b8>
2014-03-16 23:29:05 +0000

    additional InputStream based reader methods

--- src/main/java/de/rwh/utils/crypto/io/CertificateReader.java
+++ src/main/java/de/rwh/utils/crypto/io/CertificateReader.java
@@ -27,11 +27,17 @@ public final class CertificateReader
 	{
 		try (InputStream stream = Files.newInputStream(file))
 		{
-			KeyStore keyStore = KeyStore.getInstance("pkcs12");
-			keyStore.load(stream, password.toCharArray());
-
-			return keyStore;
+			return fromPkcs12(password, stream);
 		}
+	}
+
+	public static KeyStore fromPkcs12(String password, InputStream stream) throws KeyStoreException, IOException,
+			NoSuchAlgorithmException, CertificateException
+	{
+		KeyStore keyStore = KeyStore.getInstance("pkcs12");
+		keyStore.load(stream, password.toCharArray());
+
+		return keyStore;
 	}
 
 	public static KeyStore fromCer(Path file, String alias) throws NoSuchAlgorithmException, CertificateException,
@@ -39,15 +45,21 @@ public final class CertificateReader
 	{
 		try (InputStream stream = Files.newInputStream(file))
 		{
-			CertificateFactory certificateFactory = CertificateFactory.getInstance("X509");
-			Certificate certificate = certificateFactory.generateCertificate(stream);
-
-			KeyStore keyStore = KeyStore.getInstance("jks");
-			keyStore.load(null, null);
-			keyStore.setCertificateEntry(alias, certificate);
-
-			return keyStore;
+			return fromCer(alias, stream);
 		}
+	}
+
+	public static KeyStore fromCer(String alias, InputStream stream) throws CertificateException, KeyStoreException,
+			IOException, NoSuchAlgorithmException
+	{
+		CertificateFactory certificateFactory = CertificateFactory.getInstance("X509");
+		Certificate certificate = certificateFactory.generateCertificate(stream);
+
+		KeyStore keyStore = KeyStore.getInstance("jks");
+		keyStore.load(null, null);
+		keyStore.setCertificateEntry(alias, certificate);
+
+		return keyStore;
 	}
 
 	public static KeyStore allFromCer(Path file) throws NoSuchAlgorithmException, CertificateException, IOException,
@@ -55,17 +67,23 @@ public final class CertificateReader
 	{
 		try (InputStream stream = Files.newInputStream(file))
 		{
-			CertificateFactory certificateFactory = CertificateFactory.getInstance("X509");
-			Collection<? extends Certificate> certificates = certificateFactory.generateCertificates(stream);
-
-			KeyStore keyStore = KeyStore.getInstance("jks");
-			keyStore.load(null, null);
-
-			for (Certificate c : certificates)
-				keyStore.setCertificateEntry(((X509Certificate) c).getSubjectDN().getName(), c);
-
-			return keyStore;
+			return allFromCer(stream);
 		}
+	}
+
+	public static KeyStore allFromCer(InputStream stream) throws CertificateException, KeyStoreException, IOException,
+			NoSuchAlgorithmException
+	{
+		CertificateFactory certificateFactory = CertificateFactory.getInstance("X509");
+		Collection<? extends Certificate> certificates = certificateFactory.generateCertificates(stream);
+
+		KeyStore keyStore = KeyStore.getInstance("jks");
+		keyStore.load(null, null);
+
+		for (Certificate c : certificates)
+			keyStore.setCertificateEntry(((X509Certificate) c).getSubjectDN().getName(), c);
+
+		return keyStore;
 	}
 
 	private CertificateReader()
