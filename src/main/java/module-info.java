a1e417632469e7cad2cfe96516f5843d655117ee
hhund <hauke.hund@hs-heilbronn.de>
2018-03-27 14:38:03 +0200

    java 9 module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module de.rwh.utils.crypto
+{
+	exports de.rwh.utils.crypto;
+	exports de.rwh.utils.crypto.io;
+
+	requires bcprov.jdk15on;
+	requires bcpkix.jdk15on;
+	requires org.apache.commons.codec;
+	requires org.slf4j;
+}
