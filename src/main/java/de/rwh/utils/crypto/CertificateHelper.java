1d4da83cb96f1213cef26ded284ae12be62fe951
Hauke Hund <hauke.hund@hs-heilbronn.de>
2021-08-16 20:01:46 +0200

    additional helper method and test case

--- src/main/java/de/rwh/utils/crypto/CertificateHelper.java
+++ src/main/java/de/rwh/utils/crypto/CertificateHelper.java
@@ -10,6 +10,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
@@ -20,7 +21,12 @@ import java.util.function.Consumer;
 
 import javax.security.auth.x500.X500Principal;
 
+import org.bouncycastle.asn1.x500.RDN;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.asn1.x500.style.IETFUtils;
 import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
@@ -196,5 +202,13 @@ public final class CertificateHelper
 			}
 		}
 		return caCertificates;
+	}
+
+	public static String getSubjectCommonName(X509Certificate certificate) throws CertificateEncodingException
+	{
+		X500Name x500name = new JcaX509CertificateHolder(certificate).getSubject();
+		RDN cn = x500name.getRDNs(BCStyle.CN)[0];
+
+		return IETFUtils.valueToString(cn.getFirst().getValue());
 	}
 }
---
+++ src/test/java/de/rwh/utils/crypto/CertificateHelperTest.java
@@ -0,0 +1,24 @@
+package de.rwh.utils.crypto;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.junit.Test;
+
+public class CertificateHelperTest
+{
+	@Test
+	public void testCreateRsaKeyPair() throws Exception
+	{
+		KeyPair pair = CertificateHelper.createRsaKeyPair4096Bit();
+		assertNotNull(pair);
+		assertNotNull(pair.getPrivate());
+		assertNotNull(pair.getPublic());
+		assertTrue(pair.getPublic() instanceof RSAPublicKey);
+		assertTrue(pair.getPrivate() instanceof RSAPrivateKey);
+	}
+}
